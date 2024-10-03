993f21673437b30eeb5dc82434903d3dc5d31791
sokevinjonas <kjonasdevpro@gmail.com>
2024-10-03 18:09:33 +0000

    interface edit profile avec button popover

--- src/app/profile/edit-profile/edit-profile.page.ts
+++ src/app/profile/edit-profile/edit-profile.page.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-profile.page.scss'],
 })
 export class EditProfilePage implements OnInit {
+  services: string[] = [];
+  profileDescription: string =
+    'Lorem ipsum dolor sit amet consectetur adipisicing';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  addClickService() {
+    const newServiceIndex = this.services.length + 1;
+    this.services.push(
+      `Service ${newServiceIndex}: Lorem ipsum dolor sit amet consectetur.`
+    );
   }
-
+  // Supprime le service à l'index spécifié
+  deleteService(i: number) {
+    // Vérifier si l'index est valide
+    if (i > -1 && i < this.services.length) {
+      // Utiliser splice pour supprimer l'élément
+      this.services.splice(i, 1);
+    }
+  }
 }
