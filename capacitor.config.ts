20fd6c4e23f4bf9024220f80210bc45d8baf5b22
sokevinjonas <kjonasdevpro@gmail.com>
2024-10-06 04:12:19 +0000

    install et config de splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,26 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'e-profile',
-  webDir: 'www'
+  appId: 'com.naabaprofile.app',
+  appName: 'NaabaProfile',
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+      launchAutoHide: true,
+      backgroundColor: '#ffffffff',
+      androidSplashResourceName: 'splash',
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: false,
+      androidSpinnerStyle: 'large',
+      iosSpinnerStyle: 'small',
+      spinnerColor: '#999999',
+      splashFullScreen: false,
+      splashImmersive: false,
+      layoutName: 'launch_screen',
+      useDialog: false,
+    },
+  },
 };
 
 export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './services/auth.service';
 import { Users } from './interfaces/users';
-
+import { SplashScreen } from '@capacitor/splash-screen';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService) {
+    this.showSplash();
+  }
 
   ngOnInit() {
     this.authService.getInfoUser(); // Récupérer les informations de l'utilisateur
@@ -16,5 +18,11 @@ export class AppComponent implements OnInit {
       "Informations de l'utilisateur:",
       this.authService.getInfoUser()
     );
+  }
+  async showSplash() {
+    await SplashScreen.show({
+      showDuration: 3000,
+      autoHide: true,
+    });
   }
 }
